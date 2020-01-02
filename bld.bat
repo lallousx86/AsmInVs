0104f2e5a4698e250c54093aabc415ac9cd27684
Elias Bachaalany <elias.bachaalany@gmail.com>
2020-01-02 13:08:26 -0800

    Added standalone x64 assembly program

--- bld.bat
+++ bld.bat
@@ -7,21 +7,22 @@ cls
 setlocal enabledelayedexpansion
 
 set x64asm_switches=-A x64
+set x64asm_stdalone_switches=-A x64
 
 if "%1"=="" (set config=Debug) else (set config=%1)
 
-for %%a in (x86asm x64asm) do (
-	if not exist %%a goto :error
-	pushd %%a
-	if not exist build (
-		mkdir build
-		set prep=1
-	) else (
-		set prep=0
-	)
-	cd build
-	call set t=!%%a_switches!
-	if !prep! equ 1 cmake -G "Visual Studio 15" .. !t!
-	cmake --build . --config %config%
-	popd
+for %%a in (x86asm x64asm x64asm_stdalone) do (
+    if not exist %%a goto :error
+    pushd %%a
+    if not exist build (
+        mkdir build
+        set prep=1
+    ) else (
+        set prep=0
+    )
+    cd build
+    call set t=!%%a_switches!
+    if !prep! equ 1 cmake -G "Visual Studio 15" .. !t!
+    cmake --build . --config %config%
+    popd
 )
---
+++ x64asm_stdalone/stdalone_x64.asm
@@ -0,0 +1,10 @@
+.code
+
+; Standalone x64 assembly program
+
+PUBLIC mainCRTStartup
+
+mainCRTStartup:
+	mov rax, 1
+	ret
+end
